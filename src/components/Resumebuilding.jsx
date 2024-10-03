90d4b312682d86ca44f340658ab5fc4e8f1e65b4
akdon1198 <akdon1198@gmail.com>
2024-10-03 19:32:34 +0530

    added few changes

--- src/components/Resumebuilding.jsx
+++ src/components/Resumebuilding.jsx
@@ -3,6 +3,8 @@ import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
 import Header from "./Header";
 import ResumeForm from "./ResumeForm";
 import Preview from "./Preview";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Resumebuilding = () => {
   const [resumeData, setResumeData] = useState({
@@ -13,11 +15,41 @@ const Resumebuilding = () => {
     softSkills: [],
     projects: [],
   });
-
+  const navigate = useNavigate()
   const updateResumeData = (section, data) => {
     setResumeData((prevData) => ({ ...prevData, [section]: data }));
   };
-
+  const generateresume = ()=>{
+    if(resumeData.education.length == 0){
+      alert("Education is missing")
+      return
+    }
+    if(resumeData.technicalSkills.length == 0){
+      alert("Skills is missing")
+      return
+    }
+    if(resumeData.softSkills.length == 0){
+      alert("Soft skills are missing")
+      return
+    }
+    if(resumeData.projects.length == 0){
+      alert("Projects are missing")
+      return
+    }
+    console.log(resumeData);
+    axios.delete("https://resumebuilder-3edd6-default-rtdb.firebaseio.com/resumedata.json")
+    .then((val) =>{
+      return axios.post("https://resumebuilder-3edd6-default-rtdb.firebaseio.com/resumedata.json", resumeData)
+    })
+    .then((val) =>{
+      console.log(val);
+      alert("resume generated")
+      navigate("/template")
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+  }
   return (
     <ChakraProvider>
       <Container maxW="container" p={5}>
@@ -27,7 +59,9 @@ const Resumebuilding = () => {
             resumeData={resumeData}
             updateResumeData={updateResumeData}
           />
-          <Preview resumeData={resumeData} />
+          <Preview resumeData={resumeData} 
+          generateresume={generateresume}
+          />
         </Flex>
       </Container>
     </ChakraProvider>
